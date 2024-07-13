2ff33ede1f7f929f938d4ec666448ccdadb97dc5
Zeeshan Zahid <zeeshanzahid1221@gmail.com>
2024-07-13 11:20:59 +0500

    4 Program Done

---
+++ script.js
@@ -0,0 +1,14 @@
+// let num1 = Math.round(11.8);
+// console.log(num1)
+
+
+// let num1 = Math.floor("1.1");
+// console.log(num1);
+
+
+// let number1 = Math.ceil(4.1);
+// console.log(number1);
+
+
+// let randomNumber = Math.random();
+// console.log(randomNumber);
